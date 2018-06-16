b6c06102786fad1c3dfdcfeb92f6b104d948f185
Vincent Ambo <tazjin@gmail.com>
2018-06-16 20:47:22 +0200

    feat(build): Add toggle for tests to Nix derivation

--- default.nix
+++ default.nix
@@ -3,9 +3,12 @@
 # Note: This does not currently use Carnix due to an issue with
 # linking against the `systemd.dev` derivation for libsystemd.
 
-{ pkgs ? import <nixpkgs> {}}:
+{ pkgs ? import <nixpkgs> {}
+, doCheck ? true }:
 
 with pkgs; rustPlatform.buildRustPackage {
+  inherit doCheck;
+
   name        = "journaldriver";
   version     = "0.1.0";
   cargoSha256 = "165hmmsy9y7334g80yv21ma91rfavv2jk8fqssrccas7ryj4abki";
