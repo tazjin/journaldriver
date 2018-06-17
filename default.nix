208b5dec2cad72f752c091c349a5e954047b74ff
Vincent Ambo <mail@tazj.in>
2018-06-17 18:22:13 +0200

    chore: Bump to 1.0-rc1

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@ with pkgs; rustPlatform.buildRustPackage {
   inherit doCheck;
 
   name        = "journaldriver";
-  version     = "0.1.0";
+  version     = "1.0-rc1";
   cargoSha256 = "04llhriwsrjqnkbjgd22nhci6zmhadclnd8r2bw5092gwdamf49k";
 
   src = ./.;
